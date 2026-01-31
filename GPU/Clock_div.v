f5973229c3dd3c3cad53b8a8023190e56ee229a8
jenipres <jenipres@ttu.edu>
2026-01-30 21:24:19 -0600

    Rename output signal from clk_out to pixel_clk

--- GPU/Clock_div.v
+++ GPU/Clock_div.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
-module clock_div(
+module Clock_div(
     input  wire clk_in,      // 100 MHz
-    output wire clk_out       // 25 MHz
+    output wire pixel_clk       // 25 MHz
 );
     reg [1:0] clk_div = 2'd0;
 
     always @(posedge clk_in)
         clk_div <= clk_div + 1'b1;
 
-    assign clk_out = clk_div[1];
+    assign pixel_clk = clk_div[1];
 endmodule
